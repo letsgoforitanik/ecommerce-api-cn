618dcbf67e24e2d85a1e36a5ef9e19d1aa2b63c6
Aniruddha Banerjee <letsgoforitanik@gmail.com>
2023-09-16 21:32:27 +0530

    Change documentation url

--- src/controllers/api/v1/index.ts
+++ src/controllers/api/v1/index.ts
@@ -5,7 +5,7 @@ import * as productController from "./product";
 
 const v1Router = express.Router();
 
-v1Router.use('/', (req, res) => res.redirect('https://documenter.getpostman.com/view/2857534/2s9YC7SBgB'));
+v1Router.use('/', (req, res) => res.redirect('https://documenter.getpostman.com/view/2857534/2s9YC7SBgJ'));
 
 v1Router.use(productController.router);
 
